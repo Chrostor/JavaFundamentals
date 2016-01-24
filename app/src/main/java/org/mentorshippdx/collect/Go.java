14d1d5516b8ad07a8456e2f202eb5526f129677c
Matt Phillips <matt.phillips@gmail.com>
2016-01-24 13:48:20 -0800

    Continued work from with Loren's original work on Set exercise.  - modified Setting to retain the set as an instance field - added 2 examples of formatting the Set as a string in Setting class - updated Ready class to iterate over Set maintained by Setting

--- app/src/main/java/org/mentorshippdx/collect/Go.java
+++ app/src/main/java/org/mentorshippdx/collect/Go.java
@@ -4,8 +4,11 @@ package org.mentorshippdx.collect;
  * Created by pierray on 1/23/2016.
  */
 public class Go {
+
     public static void main(String[] args){
         Ready startIt = new Ready();
-        startIt.printIt();
+        Setting runList = new Setting();
+        runList.setUp();
+        startIt.printIt(runList);
     }
 }
--- app/src/main/java/org/mentorshippdx/collect/Ready.java
+++ app/src/main/java/org/mentorshippdx/collect/Ready.java
@@ -1,19 +1,23 @@
 package org.mentorshippdx.collect;
 
-import java.util.Iterator;
 
 /**
  * Created by pierray on 1/23/2016.
  */
 public class Ready {
 
+    public void printIt(Setting bookIt) {
+        // Note: letting Ready have intimate knowledge and access to the underlying
+        //       data in Setting can be questionable.  It's fine for this example but understand
+        //       it raises a flag about concerns of coupling.
 
-
-    public void printIt(){
-        Setting bookIt = new Setting();
-        //bookIt.setUp();
-        System.out.println(bookIt);
-
+        // Sets are collections making them easy to iterate over with enhanced for loop
+        for (String word : bookIt.getWords()) {
+            System.out.println("  " + word);
         }
+
+        // Alternative: let the instance format itself by use of its own toString()
+        //System.out.println("Setting is: " + bookIt); // bookIt.toString() implicitly called
     }
+}
 
